06704e731885819891452e25e615f2198d1651f7
Subu123 <saisubu987@gmail.com>
2021-04-08 00:54:21 +0530

    all Delete

--- lib/CommonClasses/service_locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:get_it/get_it.dart';
-import 'package:login/CommonClasses/calls_and_messages_services.dart';
-
-GetIt locator = GetIt.instance;
-
-void  setUpLocator() {
-  locator.registerSingleton(CallsAndMessagesServices());
-}
